201efbdb9f6ef759e132234aca058fd770cc7533
= <michiel.werbrouck@student.ucll.be>
2022-02-16 16:30:00 +0100

    01-Basics

---
+++ elixir-basics/01-basics/10-multiple-clauses-2/student.exs
@@ -0,0 +1,7 @@
+defmodule Fibonacci do
+  def fib(0), do: 0
+  def fib(1), do: 1
+  def fib(n) when is_number(n) and n >= 2 do
+    fib(n-2) + fib(n-1)
+  end
+end
---
+++ elixir-basics/01-basics/11-private-functions/student.exs
@@ -0,0 +1,8 @@
+defmodule Math do
+  defp factorial(0), do: 1
+  defp factorial(n) when is_number(n) and n > 0 do
+    n * factorial(n-1)
+  end
+
+  def binomial(n, k), do: div(factorial(n), factorial(n - k) * factorial(k))
+end
